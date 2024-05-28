ae61d74f26ba3cf8f8d2285dec5d941e00ed7e01
montenegroleticia <montenegro.letiicia@gmail.com>
2024-05-28 17:02:40 -0300

    fix: async tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,27 @@
 import pytest
-from fastapi.testclient import TestClient
+import asyncio
+from fastapi import FastAPI
+from httpx import AsyncClient
 from app.main import app
 from app.db.database import get_database
 
 @pytest.fixture(scope="module")
-def test_app():
-    client = TestClient(app)
-    yield client
+def event_loop():
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
 
 @pytest.fixture(scope="module")
-async def test_db(test_app):
+def test_app():
+    return app
+
+@pytest.fixture(scope="module")
+async def async_client(test_app: FastAPI):
+    async with AsyncClient(app=test_app, base_url="http://test") as client:
+        yield client
+
+@pytest.fixture(scope="module")
+async def test_db():
     db = get_database()
     await db.age_groups.drop()
     yield db
--- tests/test_age_group.py
+++ tests/test_age_group.py
@@ -1,9 +1,28 @@
-def test_create_age_group(test_app):
-    response = test_app.post("/age-groups/", json={"min_age": 18, "max_age": 25})
-    assert response.status_code == 200
-    assert isinstance(response.json(), str)
+import pytest
+from httpx import AsyncClient
+from app.main import app
+from app.db.database import get_database
 
-def test_get_age_groups(test_app):
-    response = test_app.get("/age-groups/")
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
+@pytest.mark.asyncio
+async def test_create_age_group():
+    db = get_database()
+    await db.age_groups.drop()
+
+    async with AsyncClient(app=app, base_url="http://test") as async_client:
+        response = await async_client.post("/age-groups/", json={"min_age": 18, "max_age": 25})
+        assert response.status_code == 200
+        assert isinstance(response.json(), str)
+
+    await db.age_groups.drop()
+
+@pytest.mark.asyncio
+async def test_get_age_groups():
+    db = get_database()
+    await db.age_groups.drop()
+
+    async with AsyncClient(app=app, base_url="http://test") as async_client:
+        response = await async_client.get("/age-groups/")
+        assert response.status_code == 200
+        assert isinstance(response.json(), list)
+
+    await db.age_groups.drop()
